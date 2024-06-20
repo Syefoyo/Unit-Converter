bb430c7339449ef075736afadeeabdfb9dd416fc
Syefoyo <trisyefolionel@gmail.com>
2024-06-20 20:13:37 +0700

    Unit Convertor Files (Website)

---
+++ script.js
@@ -0,0 +1,15 @@
+function kilo() {
+  let Miles = document.getElementById("myInput").value;  
+  const results = document.getElementById("myh2");
+  results.innerHTML = Miles * 1.60934;
+}
+function kilometer() {
+  const bro = document.getElementById("myInput").value;
+  const result = document.getElementById("myh2");
+  result.innerHTML = bro * 0.621371;
+}
+function miles() {
+  let Miless = document.getElementById("myInput").value;  
+  const resultst = document.getElementById("myh2");
+  resultst.innerHTML = Miless * 1.60934;
+}
